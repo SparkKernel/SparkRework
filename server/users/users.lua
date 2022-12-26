88b5ab69b4840c89c549433174caf060d1914d8c
frackz <65610875+frackz@users.noreply.github.com>
2022-12-26 11:52:17 +0100

    sdf

--- server/users/users.lua
+++ server/users/users.lua
@@ -5,11 +5,12 @@ function Server.Users.GetAllPlayers()
     return Server.Users
 end
 
+--- @param identifier string 
 ---@return function
 function Server.Users.Get(identifier)
     if type(identifier) ~= "string" then
         return false, "identifier_not_string"
     end
-
     
+
 end
